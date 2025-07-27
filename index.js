8c7fb9232f2fd42e74b46f9d029d4ba61e1b601f
Zuka-Dev <danazuka2003@gmail.com>
2025-07-27 22:17:10 +0100

    ***

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import adminRoutes from "./routes/admin.routes.js";
 import dotenv from "dotenv";
 import prisma from "./database/prisma.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -24,12 +25,19 @@ app.use("/api/responses", responseRoutes);
 app.use("/api/admin", adminRoutes);
 app.use(errorHandler);
 
-app.listen(port, async () => {
+// Connect to DB, then start server
+async function startServer() {
   try {
     await prisma.$connect();
     console.log("✅ Connected to database");
+
+    app.listen(port, () => {
+      console.log(`🚀 Server running at http://localhost:${port}`);
+    });
   } catch (error) {
-    console.error("❌ Database connection failed:", err);
+    console.error("❌ Database connection failed:", error);
+    process.exit(1); // Exit if DB connection fails
   }
-  console.log(`Server running at ${port}`);
-});
+}
+
+startServer();
